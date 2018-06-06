6d67ca502ed5fcdb7565ab88fccef69ac1839e7e
John Cheesman <me@johncheesman.org.uk>
2018-06-06 09:22:09 +0100

    Add build scripts

---
+++ build-docs.sh
@@ -0,0 +1,1 @@
+lua lib/ldoc/ldoc.lua ./src -d docs -p "Love2D top-down" -t "Love2D top-down documentation" -i --all
---
+++ build.sh
@@ -0,0 +1,2 @@
+rm top-down.love
+zip -9 -r top-down.love . -x \*.git\* -x \*.swp\* -x \*.sh\* -x \*.yml\* -x \*.rockspec\* -x \*.md\* -x \*.tmx\* -x \*lib/*.png\* -x\*lib/*.json\* -x \*docs\* -x \*lib/ldoc\*
